8db9978999c98bb66dd3300d13bb353d790d0f3f
Osuero <osuero@mirex.gob.do>
2021-12-24 10:40:32 -0400

    adding base controller

---
+++ BASE.API/Controllers/BaseController/BaseController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Business.Repositories;
+using Data.CommonEntities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BASE.API.Controllers.BaseController
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BaseController<T, TD> : ControllerBase
+      where T : GenericEntity
+      where TD : class
+    {
+        private readonly GenericRepository<T> _repository;
+        private readonly IMapper _mapper;
+
+        public BaseController(IMapper mapper, GenericRepository<T> repository)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public List<T> Get()
+        {
+            var result = _repository.GetAll();
+            return result.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(Guid id)
+        {
+            var objectEntity = _repository.Find(x => x.Id == id);
+            return objectEntity.ToString();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] TD body)
+        {
+            var resource = _mapper.Map<T>(body);
+
+            _repository.Add(resource);
+            _repository.Save();
+        }
+
+        [HttpPut]
+        public void Put([FromBody] T value)
+        {
+            _repository.Edit(value);
+            _repository.Save();
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody] T value)
+        {
+            _repository.Delete(value);
+            _repository.Save();
+        }
+    }
+}
